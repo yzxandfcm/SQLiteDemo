b07f64c7b17e3818b5368436f35ea2195e6a6279
Jack <yzxandfcm@163.com>
2015-10-02 13:53:37 +0800

    Create a sqlite demo
    Create a sqlite demo

---
+++ Entity Framework/Entity Framework/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework
+{
+    public class Person
+    {
+        public Int64 Id { get; set; } //注意要用Int64
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class MyContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public MyContext()
+            : base("SqliteTest")
+        {
+
+        }
+    }
+}
---
+++ Entity Framework/Entity Framework/TestForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entity_Framework
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void buttonCreateDb_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyContext context = new MyContext();
+            var empList = context.Persons.OrderBy(c => c.FirstName).ToList();
+            Console.WriteLine(empList.Count);
+
+            Person people = new Person()
+            {
+                Id = 1,
+                FirstName = "Hello",
+                LastName = "World"
+            };
+            context.Persons.Add(people);
+            context.SaveChanges();
+            Console.ReadLine();
+        }
+    }
+}
